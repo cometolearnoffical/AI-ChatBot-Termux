e373540f1a9b8f541e7c7500bc857bced5dd5f88
Come to learn <68625839+c-t-l@users.noreply.github.com>
2023-02-12 12:24:15 +0500

    Create ChatBotAI.py

---
+++ ChatBotAI.py
@@ -0,0 +1,19 @@
+import openai
+
+openai.api_key = "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"
+
+def get_response(input_text):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=input_text,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    ).get("choices", [])[0].text
+    return response
+
+while True:
+    input_text = input("You: ")
+    response = get_response(input_text)
+    print("Chatbot:", response)
